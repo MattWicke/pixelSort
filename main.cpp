381dc4f2f3a69bbe61b24e25108808b583f63067
MattWicke <mattw@industrialtechgroup.com>
2017-03-12 19:07:57 -0600

    added state machine control

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <string>
 #include "pixelSort.h"
 
 void trackBarCallback(int, void* dataPtr)
@@ -14,14 +15,31 @@ void trackBarCallback(int, void* dataPtr)
     cv::imshow("edges", cparams->edges);
 }
 
-int main(int argc, char* arg[])
+class StateMachine
 {
-    //cv::Mat input = cv::imread("lenna.jpg");
-    cv::Mat input = cv::imread("japan.jpg");
-    //cv::Mat output = cannySortColumn(input, 100);
+public:
+    StateMachine();
+    void loadSourceImages(std::string fileName);
+    void saveResults();
+    void edgeTuningPhase();
+    void displayResultsPhase();
+    void run();
+    cv::Mat processedImage;
     CannyParams cparams;
-    cparams.source = input;
+    char key;
+};
 
+StateMachine::StateMachine():
+    key(0)
+    {}
+
+void StateMachine::loadSourceImages(std::string fileName)
+{
+    cparams.source= cv::imread(fileName);
+}
+
+void StateMachine::edgeTuningPhase()
+{
     cv::namedWindow("edges", 1);
     cparams.edges = cannyEdgeDetect(cparams.source, cparams.lowThreshold);
     cv::createTrackbar("Min Threshold:"
@@ -32,11 +50,44 @@ int main(int argc, char* arg[])
                         , &cparams
                         );
     cv::imshow("edges", cparams.edges);
-    cv::waitKey(0);
-    //cv::namedWindow("sort", 1);
-    //cv::namedWindow("raw", 1);
-    //cv::imshow("sort", output);
-    //cv::imshow("raw", input);
-    //cv::imwrite("output.jpg", output );
+    key = cv::waitKey(0);
+    cv::destroyWindow("edges");
+}
+
+void StateMachine::displayResultsPhase()
+{
+    processedImage = cannySortColumn(cparams.source, cparams.lowThreshold);
+    cv::namedWindow("sorted", 1);
+    cv::imshow("sorted", processedImage);
+    key = cv::waitKey(0);
+    cv::destroyWindow("sorted");
+}
+
+void StateMachine::saveResults()
+{
+    std::string saveName;
+    std::cout << "Enter file name to save: " <<std::endl;
+    std::cin >> saveName;
+    cv::imwrite(saveName, processedImage);
+}
+
+void StateMachine::run()
+{
+    while(key != 'q')
+    {
+        edgeTuningPhase();
+        if(key =='q')
+            break;
+        displayResultsPhase();
+        if(key =='s')
+            saveResults();
+    }
+}
+
+int main(int argc, char* arg[])
+{
+    StateMachine sm;
+    sm.loadSourceImages("japan.jpg");
+    sm.run();
     return 0;
 }
